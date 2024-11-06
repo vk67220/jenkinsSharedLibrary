06a3e639ac402d2242e9967bdc6a655c27cd2be7
vk67220 <varaprasadkasturi.dba@gmail.com>
2024-11-06 22:49:15 +0530

    Update dockerHubLogin.groovy

--- vars/dockerHubLogin.groovy
+++ vars/dockerHubLogin.groovy
@@ -1,8 +1,9 @@
 // vars/dockerHubLogin.groovy
 def call(String dockerHubUserName, String dockerHubPassword) {
-    sh <<-EOF
+    // Use the `steps.sh` to ensure it runs in the Pipeline context
+    steps.sh """
         echo "-------- Logging to DockerHub --------"
-        echo "$dockerHubPassword" | docker login -u "$dockerHubUserName" --password-stdin
+        echo "${dockerHubPassword}" | docker login -u "${dockerHubUserName}" --password-stdin
         echo "-------- Successfully Logged in --------"
-    EOF
+    """
 }
