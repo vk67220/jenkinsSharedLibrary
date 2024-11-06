19ec442b549d74a7abc667f5a2a94595a5230e16
vk67220 <varaprasadkasturi.dba@gmail.com>
2024-11-06 17:39:31 +0530

    Update dockerHubLogin.groovy

--- vars/dockerHubLogin.groovy
+++ vars/dockerHubLogin.groovy
@@ -1,7 +1,9 @@
+// vars/dockerHubLogin.groovy
 def call(String dockerHubUserName, String dockerHubPassword) {
-  sh """
-    echo "-------- Logging to DockerHub --------"
-    docker login -u ${dockerHubUserName} --password ${dockerHubPassword}
-    echo "-------- Successfully Loggedin --------"
-  """
+    sh """
+        echo "-------- Logging to DockerHub --------"
+        echo "${dockerHubPassword}" | docker login -u ${dockerHubUserName} --password-stdin
+        echo "-------- Successfully Logged in --------"
+    """
 }
+
