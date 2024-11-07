419cdada4648df25808773c8600ae3785258f683
vk67220 <varaprasadkasturi.dba@gmail.com>
2024-11-07 21:06:17 +0530

    Update dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,7 +1,7 @@
 def call() {
     withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         sh """
-        docker login --username="${DOCKER_USERNAME}" --password="${DOCKER_PASSWORD}"
+        echo "${DOCKER_PASSWORD}" | docker login --username="${DOCKER_USERNAME}" --password-stdin
         """
     }
 }
